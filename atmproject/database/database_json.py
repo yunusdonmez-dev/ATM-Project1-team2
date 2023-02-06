1e342d0c045f6e13798fd8149116faaf70d58915
yunus-dev <119734741+yunusnl@users.noreply.github.com>
2023-02-06 20:33:37 +0100

    add_changes_yunus
    Co-Authored-By: Tuba GÜMÜS ESMEK <119704176+tubagumus@users.noreply.github.com>
    Co-Authored-By: Ali Camoglu <119692910+alicamoglu@users.noreply.github.com>
    Co-Authored-By: Yakup Atas <119791076+Ykpython@users.noreply.github.com>

--- atmproject/database/database_json.py
+++ atmproject/database/database_json.py
@@ -3,13 +3,13 @@ import json
 import os 
 fileloc = os.getcwd()
 print(fileloc)
-def write_json(data,filename = f"{fileloc}\database\data2.json"):
+def write_json(data,filename = r"C:\Users\yunus\OneDrive\Desktop\python ogreniyorum\ATM-Project\atmproject\atm_proje_file\data2.json"):
     with open(filename,'w') as f:
         json.dump(data , f, indent=4)
 
-with open(f"{fileloc}\database\data2.json") as json_file:
+with open(r"C:\Users\yunus\OneDrive\Desktop\python ogreniyorum\ATM-Project\atmproject\atm_proje_file\data2.json") as json_file:
     data = json.load(json_file)
     temp = data["customers"]
-    y = {"id": len(temp)+1,"name": "yakup","password":32,"balance":8986589}
+    y = {"id": len(temp)+1,"name": "Tuba","password":"azra","balance":"99999"}
     temp.append(y)
 write_json(data)
